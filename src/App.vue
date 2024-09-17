bebe0d7b3c409cbdeb205759b4adac66361e09a4
Roy Fernandez <u20221b778@upc.edu.pe>
2024-09-17 02:48:25 -0500

    feat(FAQ2): added the necessary attributes for the FAQ

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Preguntas Frecuentes(FAQ)</h1>
+    <FrequentlyQuestionsComponent />
+  </div>
+</template>
+
+<script>
+import FrequentlyQuestionsComponent from './the-informations/components/frequently-questions.component.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FrequentlyQuestionsComponent
+  }
+};
+</script>
+
+<style>
+
+</style>
