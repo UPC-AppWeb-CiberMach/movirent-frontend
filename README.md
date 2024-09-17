7b348b1a59eb20d0107221c60760247dda6e2fa6
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-09-17 00:55:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### dev-frontend
+
+install See [PrimeVue](https://vitejs.dev/config/).
+
+```sh
+npm install primevue
+```
+
+install Styled Mode
+
+```sh
+npm install @primevue/themes
+```
+
+Icons
+
+```sh
+npm install primeicons
+```
---
+++ src/main.js
@@ -0,0 +1,50 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css'
+
+import Toolbar from 'primevue/toolbar';
+import Button from "primevue/button";
+import Avatar from 'primevue/avatar';
+import Carousel from 'primevue/carousel';
+import Tag from 'primevue/tag';
+import DataView from 'primevue/dataview';
+import SelectButton from 'primevue/selectbutton';
+import Chip from 'primevue/chip';
+import Card from 'primevue/card';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import Checkbox from 'primevue/checkbox';
+import InputText from 'primevue/inputtext';
+import Divider from 'primevue/divider';
+import FloatLabel from 'primevue/floatlabel';
+import Password from 'primevue/password';
+
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('pv-toolbar', Toolbar);
+app.component('pv-button', Button);
+app.component('pv-avatar', Avatar);
+app.component('pv-carousel', Carousel);
+app.component('pv-tag', Tag);
+app.component('pv-dataview', DataView);
+app.component('pv-selectbutton', SelectButton);
+app.component('pv-chip', Chip);
+app.component('pv-card', Card);
+app.component('pv-iconfield', IconField);
+app.component('pv-inputicon', InputIcon);
+app.component('pv-checkbox', Checkbox);
+app.component('pv-inputtext', InputText);
+app.component('pv-divider', Divider);
+app.component('pv-floatlabel', FloatLabel);
+app.component('pv-password', Password);
+app.mount('#app')
