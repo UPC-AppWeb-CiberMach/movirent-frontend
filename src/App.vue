701b8281131ff1cd3439e8c1aae20fc5677cbcd5
Adriana Diestra <adriana10tra@gmail.com>
2024-09-18 23:35:22 -0500

    feat: add a card to create and delete scooters

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import TheHeader from "@/layouts/component/the-header.component.vue"
+import TheCarScooters from "@/vehicule-management/components/the-card-scooter.component.vue"
 </script>
 <template>
   <TheHeader/>
+  <TheCarScooters></TheCarScooters>
 </template>
---
+++ src/vehicule-management/components/the-card-scooter.component.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card-container" >
+    <div class="card">
+      <div class="main-card">
+        <h2 class="title">Mis Scooters</h2>
+        <pv-button label="Agregar Nuevo Scooter" icon="pi pi-plus" iconPos="right" class="p-button-primary" />
+      </div>
+      <div>
+        <h3 style="padding-top: 40px">Aun no tienes registros de tus Scooter</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+}
+
+.card {
+  width: 100%;
+  max-width: 800px;
+  min-height: 400px;
+  padding: 30px;
+  background-color: #bfbfbf;
+  border-radius: 20px;
+  text-align: center;
+  color: #513c86;
+}
+
+.main-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #513c86;
+}
+</style>
+<script setup lang="ts">
+</script>
