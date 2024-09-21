48ca641bbd047789ae1d0dcdeac02904ee93bd98
MesSobble <u201911727@upc.edu.pe>
2024-09-21 14:05:50 -0500

    feat: add validator in register form

--- src/profile-management/components/register-form.component.vue
+++ src/profile-management/components/register-form.component.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="sign-up-container" aria-labelledby="sign-up-heading">
     <h1 id="sign-up-heading" class="font-bold">Regístrate y sé parte de esta gran comunidad</h1>
-    <pv-inputtext v-model="completeName" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Nombre Completo" aria-label="Nombre Completo"/>
-    <pv-inputtext v-model="password" class="w-15rem lg:w-25rem p-3" type="password" placeholder="Contraseña" aria-label="Contraseña"/>
-    <pv-inputtext v-model="phone" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Celular" aria-label="Número de Celular"/>
-    <pv-inputtext v-model="email" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Correo" aria-label="Correo Electrónico"/>
-    <pv-inputtext v-model="dni" class="w-15rem lg:w-25rem p-3" type="text" placeholder="DNI" aria-label="Documento de Identidad"/>
+    <pv-inputtext v-model="completeName" :class="{ 'is-invalid': $v.completeName.$error }" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Nombre Completo" aria-label="Nombre Completo"/>
+    <pv-inputtext v-model="password" :class="{ 'is-invalid': $v.password.$error }" class="w-15rem lg:w-25rem p-3" type="password" placeholder="Contraseña" aria-label="Contraseña"/>
+    <pv-inputtext v-model="phone" :class="{ 'is-invalid': $v.phone.$error }" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Celular" aria-label="Número de Celular"/>
+    <pv-inputtext v-model="email" :class="{ 'is-invalid': $v.email.$error }" class="w-15rem lg:w-25rem p-3" type="text" placeholder="Correo" aria-label="Correo Electrónico"/>
+    <pv-inputtext v-model="dni" :class="{ 'is-invalid': $v.dni.$error }" class="w-15rem lg:w-25rem p-3" type="text" placeholder="DNI" aria-label="Documento de Identidad"/>
 
     <pv-button @click="signUp" class="mt-5 p-4 w-12rem sign-up-btn" type="submit" label="Regístrate" aria-label="Botón para registrarse"/>
 
@@ -23,35 +23,59 @@
 import {Db} from "@/profile-management/services/user.services.js";
 import { v4 as uuidv4 } from 'uuid';
 import router from "@/routes/router.js";
+import { ref, reactive } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, minLength, email as emailValidator } from '@vuelidate/validators'
+
 let id = uuidv4();
-let completeName = "";
-let password = "";
-let phone = "";
-let email = "";
-let dni = "";
+let completeName = ref('')
+let password = ref('')
+let phone = ref('')
+let email = ref('')
+let dni = ref('')
+
+const rules = reactive({
+  completeName: { required },
+  password: { required, minLength: minLength(8) },
+  email: { required, emailValidator },
+  phone: { required, minLength: minLength(9), type: Number },
+  dni: { required, minLength: minLength(8) }
+})
+
+const $v = useVuelidate(rules, { completeName, password, email, phone, dni })
 
 async function signUp(){
-  let user = {
-    id: id,
-    email: email,
-    password: password,
-    completeName: completeName,
-    phone: phone,
-    dni : dni,
-    user_roles_id : 1
+  $v.value.$touch()
+  if ($v.value.$error) {
+    console.log("Error")
   }
-  await Db.prototype.signUp(user).then((response) => {
-    if(response.status === 201){
-      alert("Sign Up Success");
-      router.push("/login")
+  else {
+
+    let user = {
+      id: id,
+      email: email.value,
+      password: password.value,
+      completeName: completeName.value,
+      phone: phone.value,
+      dni: dni.value,
+      user_roles_id: 1
     }
-  }).catch(() => {
-    alert("Error");
-  });
+    await Db.prototype.signUp(user).then((response) => {
+      if (response.status === 201) {
+        alert("Sign Up Success");
+        router.push("/login")
+      }
+    }).catch(() => {
+      alert("Error");
+    });
+  }
 }
 </script>
 
 <style scoped>
+.is-invalid {
+  border-color: red;
+}
 .sign-up-container{
   display: flex;
   flex-direction: column;
