8bd60e0d77e622ec63b152e06ce42ce869fccd35
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-21 16:07:43 -0500

    update(view-history): add second version data view reservation history and update db.json data

--- src/profile-management/services/history-services.js
+++ src/profile-management/services/history-services.js
@@ -1,10 +1,10 @@
 import {BaseService} from "@/shared/base.service.js";
 
 export class HistoryServices{
-    getHistory(){
-        return BaseService.http.get("/reservations")
-    }
-    getHistoryById(clientId){
+    getHistoryByClientId(clientId){
         return BaseService.http.get(`/reservations?client_id=${clientId}`);
+    }
+    getScooterByScooterId(scooterId){
+        return BaseService.http.get(`/scooters/${scooterId}`);
     }
 }
