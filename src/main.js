8ef3890554af3c0d82d5f947aa833164da452e3a
MesSobble <u201911727@upc.edu.pe>
2024-09-21 17:23:04 -0500

    Revert "Merge branch 'feature/profile' of https://github.com/UPC-AppWeb-CiberMach/front-end into feature/profile"
    This reverts commit 430ac8952e07c49c19e89b2610da42c63d7432a5, reversing
    changes made to 3228db5b471b0b42421790623bd549209dac83d1.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css';
 
 import Toolbar from 'primevue/toolbar';
 import Button from "primevue/button";
