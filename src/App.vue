7cd3389a4791aa701b34559402678694d1a71273
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-09-23 01:25:59 -0500

    feat: add App.vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+/* General Styling */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f4f4f4;
+}
+
+h2 {
+  font-weight: bold;
+  color: #2f2f2f;
+}
+
+/* Styling for full page layout */
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+footer {
+  margin-top: auto;
+}
+</style>
