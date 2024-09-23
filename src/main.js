fc7d83f5296ea52650f542cf3a4612d5f39480d3
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-09-23 01:27:35 -0500

    feat: add main.js

--- src/main.js
+++ src/main.js
@@ -1,50 +1,45 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import 'primeicons/primeicons.css'
+import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
 
-import Toolbar from 'primevue/toolbar';
-import Button from "primevue/button";
-import Avatar from 'primevue/avatar';
-import Carousel from 'primevue/carousel';
-import Tag from 'primevue/tag';
-import DataView from 'primevue/dataview';
-import SelectButton from 'primevue/selectbutton';
-import Chip from 'primevue/chip';
-import Card from 'primevue/card';
-import IconField from 'primevue/iconfield';
-import InputIcon from 'primevue/inputicon';
-import Checkbox from 'primevue/checkbox';
-import InputText from 'primevue/inputtext';
-import Divider from 'primevue/divider';
-import FloatLabel from 'primevue/floatlabel';
-import Password from 'primevue/password';
+// Importar los estilos de PrimeVue
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
+// Importar vue-router
+import { createRouter, createWebHistory } from 'vue-router';
+import ScooterList from './application/screens/Scooter-List.component.vue';
+import ScooterDetail from './application/screens/Scooter-Detail.component.vue';
+import ScooterReviews from './application/screens/Scooter-Reviews.component.vue';
+import ScooterAddReview from './application/screens/Scooter-Add-Review.component.vue'; // Importar el componente
 
-const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
+// Definir las rutas
+const routes = [
+    { path: '/', component: ScooterList }, // Pantalla de scooters
+    { path: '/scooter/:id', component: ScooterDetail }, // Pantalla de detalle del scooter
+    { path: '/scooter/:id/reviews', component: ScooterReviews }, //  Pantalla de reseña del scooter
+    { path: '/scooter/:id/add-review', component: ScooterAddReview } // Pantalla de dejar reseña al scooter
+];
+
+// Crear el router
+const router = createRouter({
+    history: createWebHistory(),
+    routes
 });
 
-app.component('pv-toolbar', Toolbar);
-app.component('pv-button', Button);
-app.component('pv-avatar', Avatar);
-app.component('pv-carousel', Carousel);
-app.component('pv-tag', Tag);
-app.component('pv-dataview', DataView);
-app.component('pv-selectbutton', SelectButton);
-app.component('pv-chip', Chip);
-app.component('pv-card', Card);
-app.component('pv-iconfield', IconField);
-app.component('pv-inputicon', InputIcon);
-app.component('pv-checkbox', Checkbox);
-app.component('pv-inputtext', InputText);
-app.component('pv-divider', Divider);
-app.component('pv-floatlabel', FloatLabel);
-app.component('pv-password', Password);
-app.mount('#app')
+// Crear la aplicación
+const app = createApp(App);
+
+// Usar PrimeVue
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('Dropdown', Dropdown);
+
+// Usar el router
+app.use(router);
+
+// Montar la aplicación
+app.mount('#app');
