dd34f461f896b8f469010deefb0ef394e29a9f98
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-09-23 01:24:43 -0500

    feat: add card-and-filter-scooter

---
+++ src/ui/components/District-Filter.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <Dropdown :options="districts" v-model="selectedDistrict" placeholder="Distrito" @change="$emit('filter')" />
+</template>
+
+<script>
+import Dropdown from 'primevue/dropdown';
+
+export default {
+  components: {
+    Dropdown
+  },
+  data() {
+    return {
+      selectedDistrict: null,
+      districts: [
+        { label: 'Distrito 1', value: 'Distrito 1' },
+        { label: 'Distrito 2', value: 'Distrito 2' }
+      ]
+    };
+  }
+};
+</script>
