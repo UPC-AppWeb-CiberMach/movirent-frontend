bab78f0a774c4e2f35ac04e76ae81cd5071c3e24
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-09-23 15:42:53 -0500

    fix: Code has been removed to make merging with other branches more efficient.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,12 @@
-import {createRouter} from "vue-router";
-import {createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
+
+import ReservationHistory from "../profile-management/components/reservation-history.component.vue";
+import ReservationDetailsDialog from "../profile-management/components/reservation-details-dialog.component.vue";
+
 
 const routes = [
-    { path:"/", component: () => import("../profile-management/components/register-form.component.vue")},
-    { path: '/register', component: () => import('../profile-management/components/register-form.component.vue') },
-    { path: '/login', component: () => import('../profile-management/components/login-form.component.vue') },
-    { path: '/profile', component: () => import('../profile-management/components/user-view.component.vue') },
-    {path: '/history', component: () => import('../profile-management/components/reservation-history.component.vue')},
-    { path: '/reservation/:id', component: () => import('../profile-management/components/reservation-details-dialog.component.vue') },
+    {path: '/', component: ReservationHistory },
+    { path: '/reservation/:id', component: ReservationDetailsDialog }
 ]
 
 const router = createRouter({
