ca4cf062e11f2e11f913e429aecfa131a5e6063e
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-10-26 21:58:04 -0500

    feat: Improvement in the development of styles, login and user presentation.

--- src/movirent/profile-management/components/login-form.component.vue
+++ src/movirent/profile-management/components/login-form.component.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="p-d-flex p-flex-column p-ai-center p-jc-center sign-in-container" style="margin-top: 10%; width: 100%; max-width: 25rem; margin-left: auto; margin-right: auto;" aria-label="Contenedor de inicio de sesión">
-    <h1 id="sign-in-heading" class="font-bold pv-txt" aria-label="Título de inicio de sesión">Iniciar Sesión</h1>
-    <pv-inputtext v-model="email" class="w-15rem lg:w-25rem p-3 component-margin pv-text" type="text" placeholder="Email" aria-label="Campo de correo electrónico"/>
-    <pv-inputtext v-model="password" class="w-15rem lg:w-25rem p-3 component-margin pv-text" type="password" placeholder="Contraseña" aria-label="Campo de contraseña"/>
-    <div class="button-container">
-      <pv-button @click="signIn" class="mt-5 p-2 sign-in-btn component-margin" type="submit" label="Iniciar Sesión" aria-label="Botón para iniciar sesión"/>
-    </div>
-    <div class="grid-container component-margin" aria-label="Sección de registro">
-      <h3 aria-label="Pregunta de registro" class="pv-txt">¿Aún no tienes una cuenta?</h3>
-      <div class="register-redirect" aria-label="Redirección de registro">
-        <router-link to="/register" class="sign-up-section" aria-label="Enlace para registrarse">Regístrate</router-link>
+  <div class="container">
+    <div class="pv-card" aria-label="Contenedor de inicio de sesión">
+
+      <div class="text" aria-label="Título de inicio de sesión">Iniciar Sesión</div>
+
+      <label for="username" class="title-input">Usuario</label> <br/>
+      <pv-inputtext
+          v-model="email"
+          class="w-15rem lg:w-18rem p-2 pv-text-input"
+          type="type"
+          placeholder="Email"
+          aria-label="Campo de correo electrónico"
+      /> <br/> <br/>
+
+
+      <label for="username" class="title-input">Contraseña</label> <br/>
+      <pv-inputtext
+          v-model="password"
+          class="w-15rem lg:w-18rem p-2 pv-text-input"
+          type="password"
+          placeholder="Contraseña"
+          aria-label="Campo de contraseña"
+      /> <br/><br/>
+
+
+      <div class="button-container">
+        <pv-button
+            @click="signIn"
+            type="submit"
+            class="p-2 sign-in-btn"
+            label="Iniciar Sesión"
+            severity="success"
+            aria-label="Botón para iniciar sesión" />
+      </div>  <br/>
+
+
+      <div class="grid-container component-margin" aria-label="Sección de registro">
+        <h3 aria-label="Pregunta de registro" class="pv-txt">¿Aún no tienes una cuenta?</h3>
+        <div class="register-redirect" aria-label="Redirección de registro">
+          <router-link to="/register" class="sign-up-section" aria-label="Enlace para registrarse">Regístrate</router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -39,54 +69,51 @@ async function signIn() {
 </script>
 
 <style scoped>
-.pv-text{
-  background-color: #ffffff;
-  color: black;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
+.pv-card {
+  background-color: #fff;
+  border-radius: 12px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+  padding: 2.5rem;
+}
+.text {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #282828;
+}
+.pv-text-input{
+  background-color: #ffffff;
+  color: #575656;
+  border: 1px solid #818080;
+}
+.title-input{
+  font-size: 0.7rem;
+  color: #919090;
+  text-align: center;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+.sign-in-btn{
+  border: none;
+}
+
 .pv-txt{
   color: black;
 }
-.sign-in-container{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 300px;
-  width: 100%;
-  max-width: 25rem;
-  margin-left: auto;
-  margin-right: auto;
-}
 
-.font-bold {
-  text-align: center;
-}
-
-.component-margin {
-  margin-bottom: 20px;
-  margin-top: 20px;
-}
 .grid-container {
   display: grid;
   grid-template-columns: auto auto;
   justify-content: center;
   gap: 2px;
-}
-.button-container {
-  display: flex;
-  justify-content: center;
-  width: 50%;
-}
-
-.sign-in-btn{
-  background-color: #72D063;
-  border: none;
-  height: 3rem;
-  width: 9rem;
-}
-
-.sign-in-btn:hover{
-  background-color: #5cbf4b;
 }
 
 .register-redirect {
