4f7453a3b538b85498e17b127175cef49f727787
QuiqueVladimir <U202022365@upc.edu.pe>
2024-11-18 16:31:19 -0500

    feat(api): add connection to api fake

--- src/movirent/profile-management/services/user.services.js
+++ src/movirent/profile-management/services/user.services.js
@@ -1,8 +1,8 @@
 import {BaseService} from "@/shared/base.service.js"
 
 export class Db {
-    signIn(email,password) {
-        return BaseService.http.get("/user" + "/" + "?email=" + email + "&password=" + password);
+    signIn(email, password) {
+        return BaseService.http.get(`/user/signin?email=${email}&password=${password}`);
     }
     signUp(user){
         return BaseService.http.post("/user",user)
--- src/movirent/vehicule-management/services/scooter.services.js
+++ src/movirent/vehicule-management/services/scooter.services.js
@@ -4,18 +4,18 @@ import {BaseService} from "@/shared/base.service.js";
 export class Db {
 
     getAllScooters() {
-        return BaseService.http.get("/scooter");
+        return BaseService.http.get("/Scooter");
     }
     createScooter(scooter){
-        return BaseService.http.post("/scooter",scooter)
+        return BaseService.http.post("/Scooter",scooter)
     }
 
     editScooter(id,scooter){
-        return BaseService.http.put("/scooter/" + id,scooter)
+        return BaseService.http.put("/Scooter/" + id,scooter)
     }
 
     deleteScooter(id){
-        return BaseService.http.delete("/scooter/" + id)
+        return BaseService.http.delete("/Scooter/" + id)
     }
 
     createReservation(reservation) {
